5259a865eb90d4c9ffb33c35fe4e6c30beff133d
Yusuf Dede <yusufdede95@hotmail.com>
2022-06-11 16:44:11 +0300

    last refactor

---
+++ README.md
@@ -0,0 +1,12 @@
+# ts-corpus-mld-dl
+- ai concepts with ML & DL test TTC-3600 Datasets
+
+#### Install Process
+```shell
+    $ pip install -r requirements.txt
+```
+
+#### So run!
+```shell
+    $ python tests.py
+```
--- tests.py
+++ tests.py
@@ -1,18 +1,25 @@
 import time
+from pprint import pprint
 
 from ai_concepts.deep_learning.dl_classifiers import DLClassifiers
 from ai_concepts.machine_learning.ml_classifiers import MLClassifiers
-from src.text_processing_wizard import TextProcessingWizard
+from utils.text_processing_wizard import TextProcessingWizard
+import warnings
+
+warnings.warn('ignore')
 
 if __name__ == '__main__':
     text_processing_wizard = TextProcessingWizard()
 
     start_time = time.perf_counter()
+
+    print("ML Classifiers - RUNNING..")
     ml_classifiers = MLClassifiers(df_out=text_processing_wizard.get_dataframe())
     print(ml_classifiers.call_ml_classifiers())
 
+    print("DL Classifiers - RUNNING..")
     dl_classifiers = DLClassifiers(df_out=text_processing_wizard.get_dataframe())
-    print(dl_classifiers.test_text('bilim'))
+    pprint(dl_classifiers.test_text('bilim'))
     finish_time = time.perf_counter()
 
     print(f"Finished in {round(finish_time - start_time, 3)} seconds")
